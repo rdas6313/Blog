a86ea579a10fe7c92a0d9295aa3465116eb181ce
Raja <rdas6313@gmail.com>
2023-09-06 00:15:50 +0530

    Implemented blog list on index page

--- blog_app/views.py
+++ blog_app/views.py
@@ -4,3 +4,15 @@ from .models import Author, Post
 from django.db.models import F, Count, Max
 from datetime import datetime
 # Create your views here.
+
+
+def index(request):
+    queryset = Post.objects.select_related("author").all()
+    context = {
+        "posts": queryset
+    }
+    return render(request, 'blog_app/blog_post_list.html', context)
+
+
+def detail(request, id):
+    return render(request, 'blog_app/blog_post_detail.html')
