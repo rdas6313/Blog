c96dec4dde8a6d8b08c86a4d7abbdb5df7ba279b
Raja <rdas6313@gmail.com>
2023-09-09 22:10:06 +0530

    Implemented tag selecting in backend

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Post, Tag, Author
-
+from django.db.models import Count
 # Create your views here.
 
 
 def index(request):
     """ This function is responsible for index page """
+    selected_tag_list = request.GET.getlist('list_params[]')
+
     posts_queryset = Post \
         .objects \
         .select_related(
             "author") \
-        .all() \
-        .order_by('-date')
+        .filter(tag__isnull=False) \
+        .distinct() \
+        .order_by('-date') \
 
-    tags_queryset = Tag.objects.filter(post__in=posts_queryset).distinct()
+    latest_post_queryset = posts_queryset
+
+    if selected_tag_list:
+        posts_queryset = posts_queryset.filter(tag__in=selected_tag_list)
+
+    tag_queryset = Tag.objects.exclude(post__isnull=True)
 
     paginator = Paginator(posts_queryset, 6)
     page_number = request.GET.get('page', 1)
@@ -22,8 +30,8 @@ def index(request):
 
     context = {
         "posts": page_obj,
-        "latest_post": posts_queryset[:6],
-        "tags": tags_queryset
+        "latest_post": latest_post_queryset[:6],
+        "tags": tag_queryset
     }
     return render(request, 'blog_app/blog_post_list.html', context)
 
